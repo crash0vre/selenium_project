da5369812627f8aed24842b7970e4ae37accd209
crash0vre <crash0vre@users.noreply.github.com>
2022-05-10 13:36:11 -0400

    web elements

--- WDriver/webdriver_class.py
+++ WDriver/webdriver_class.py
@@ -6,10 +6,10 @@ import time
 
 #initializing the chrome driver
 driver=webdriver.Chrome()
-driver.maximize_window()
+#driver.maximize_window()
 driver.implicitly_wait(10) #max wait time for all find elements
 #properties for
-driver.get('https://demoqa.com/browser-windows')
+"""driver.get('https://demoqa.com/browser-windows')
 
 time.sleep(2)
 print('Current url: ',driver.current_url)
@@ -31,11 +31,27 @@ driver.switch_to.window(driver.window_handles[-1])
 
 print('Current URL: ',driver.current_url)
 time.sleep(3)
-driver.close()
-time.sleep(2)
+"""
+
 driver.get('https://demoqa.com/alerts')
 time.sleep(2)
-driver.find_element(By.ID,'alertButton').click()
+driver.find_element(By.XPATH,"//button[@id='alertButton']").click()
+alrt= driver.switch_to.alert
+time.sleep(2)
+alrt.accept() #clicking Ok BUtton
+driver.find_element(By.XPATH,"//button[@id='confirmButton']").click()
+alrt=driver.switch_to.alert
+time.sleep(2)
+alrt.dismiss()
+driver.find_element(By.XPATH, "//button[@id='promtButton']").click()
+alrt=driver.switch_to.alert
+alrt.send_keys("Hello world")
+time.sleep(3)
+alrt.accept()
+print('driver name: ',driver.name)
+print('Drive title: ',driver.title)
+driver.close()
+#driver.find_element(By.ID,'alertButton').click()
 #driver.switch_to.window(driver.window_handles[-1])
 
 #driver.quit()
